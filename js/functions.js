b3c7ebd4bb9a5197a2c6066248f514d295c43d3c
Mariusz Dabrowski <wayrse@gmail.com>
2015-09-23 19:24:30 -0400

    Updated drawers
    - Drawers now have numbered alphabetical lists of all the countries.

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,26 @@
 // Variables
 // Learn to not make these global but be accessible from other files (namespace)
-var countryArray = [],
-correctAnswers = [],
+var correctAnswers = [],
 answerCount = 0,
 gamseStatus = false,
 independent = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'The Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Republic of the Congo', 'Democratic Republic of the Congo', 'Costa Rica', 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'The Gambia', 'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Ivory Coast', 'Iran', 'Iraq', 'Republic of Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Republic of Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico', 'Federated States of Micronesia', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Korea', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Republic of Kosovo', 'Romania', 'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'São Tomé and Príncipe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Korea', 'South Sudan', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'East Timor', 'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vatican City', 'Vanuatu', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe'],
-
-countNorthAmerica = 0,
-countSouthAmerica = 0,
-countEurope = 0,
-countAsia = 0,
-countOceania = 0,
-countAfrica = 0,
+continents = ['north-america', 'south-america', 'europe', 'asia', 'oceania', 'africa'],
 
 countCorrectNorthAmerica = 0,
 countCorrectSouthAmerica = 0,
 countCorrectEurope = 0,
 countCorrectAsia = 0,
 countCorrectOceania = 0,
-countCorrectAfrica = 0;
+countCorrectAfrica = 0,
+
+listNorthAmerica = [],
+listSouthAmerica = [],
+listEurope = [],
+listAsia = [],
+listOceania = [],
+listAfrica = [],
+
+countryArray = [];
 
 function updateWithAnswer (count, region) {
   count += 1;
@@ -27,9 +29,6 @@ function updateWithAnswer (count, region) {
 }
 
 $(document).ready(function(){
-
-  // Initialize the nano scroller
-  $(".nano").nanoScroller();
 
   // Retreive the country info from the JSON file
   $.ajax({
@@ -41,33 +40,56 @@ $(document).ready(function(){
         currentItemCode = data[i].name;
 
         if(jQuery.inArray(currentItemCode, independent) !== -1) {
-          countryArray.push(currentItem);
-
           if (currentItem.subregion == 'Northern America' || currentItem.subregion == 'Central America' || currentItem.subregion == 'Caribbean') {
-            countNorthAmerica += 1;
+            listNorthAmerica.push(currentItem);
           } else if (currentItem.subregion == 'South America') {
-            countSouthAmerica += 1;
+            listSouthAmerica.push(currentItem);
           } else if (currentItem.region == 'Europe') {
-            countEurope += 1;
+            listEurope.push(currentItem);
           } else if (currentItem.region == 'Asia') {
-            countAsia += 1;
+            listAsia.push(currentItem);
           } else if (currentItem.region == 'Oceania') {
-            countOceania += 1;
+            listOceania.push(currentItem);
           } else if (currentItem.region == 'Africa') {
-            countAfrica += 1;
+            listAfrica.push(currentItem);
           }
         }
+
+        countryArray = listNorthAmerica.concat(listSouthAmerica, listEurope, listAsia, listOceania, listAfrica);
 
         $('.count-total').text(countryArray.length);
       }
 
       // Insert the totals into the categories
-      $('.continent-list-item[data-region="north-america"] .continent-count-total').text(countNorthAmerica);
-      $('.continent-list-item[data-region="south-america"] .continent-count-total').text(countSouthAmerica);
-      $('.continent-list-item[data-region="europe"] .continent-count-total').text(countEurope);
-      $('.continent-list-item[data-region="asia"] .continent-count-total').text(countAsia);
-      $('.continent-list-item[data-region="oceania"] .continent-count-total').text(countOceania);
-      $('.continent-list-item[data-region="africa"] .continent-count-total').text(countAfrica);
+      $('.continent-list-item[data-region="north-america"] .continent-count-total').text(listNorthAmerica.length);
+      $('.continent-list-item[data-region="south-america"] .continent-count-total').text(listSouthAmerica.length);
+      $('.continent-list-item[data-region="europe"] .continent-count-total').text(listEurope.length);
+      $('.continent-list-item[data-region="asia"] .continent-count-total').text(listAsia.length);
+      $('.continent-list-item[data-region="oceania"] .continent-count-total').text(listOceania.length);
+      $('.continent-list-item[data-region="africa"] .continent-count-total').text(listAfrica.length);
+
+      // Populate the country lists
+      for (var i = 0; i < listNorthAmerica.length; i++) {
+        $('.info-drawer[data-region="north-america"] .country-list').append('<li class="country-list-item">&nbsp;</li>');
+      }
+      for (var i = 0; i < listSouthAmerica.length; i++) {
+        $('.info-drawer[data-region="south-america"] .country-list').append('<li class="country-list-item">&nbsp;</li>');
+      }
+      for (var i = 0; i < listEurope.length; i++) {
+        $('.info-drawer[data-region="europe"] .country-list').append('<li class="country-list-item">&nbsp;</li>');
+      }
+      for (var i = 0; i < listAsia.length; i++) {
+        $('.info-drawer[data-region="asia"] .country-list').append('<li class="country-list-item">&nbsp;</li>');
+      }
+      for (var i = 0; i < listOceania.length; i++) {
+        $('.info-drawer[data-region="oceania"] .country-list').append('<li class="country-list-item">&nbsp;</li>');
+      }
+      for (var i = 0; i < listAfrica.length; i++) {
+        $('.info-drawer[data-region="africa"] .country-list').append('<li class="country-list-item">&nbsp;</li>');
+      }
+
+      // Initialize the nano scroller
+      $(".nano").nanoScroller();
 
     }
   });
@@ -103,33 +125,27 @@ $(document).ready(function(){
             if (currentItem.subregion == 'Northern America' || currentItem.subregion == 'Central America' || currentItem.subregion == 'Caribbean') {
               countCorrectNorthAmerica += 1;
               $('.continent-list-item[data-region="north-america"] .continent-count-right').text(countCorrectNorthAmerica);
-              $('.info-drawer[data-region="north-america"] .country-list').append('<li class="country-list-item">' + inputValue + '</li>');
-              $('.info-drawer[data-region="north-america"] .info-drawer-message').hide();
+              $('.info-drawer[data-region="north-america"] .country-list-item:eq(' + listNorthAmerica.indexOf(currentItem) + ')').append('<span class="country-list-item-text">' + inputValue + '</span>');
             } else if (currentItem.subregion == 'South America') {
               countCorrectSouthAmerica += 1;
               $('.continent-list-item[data-region="south-america"] .continent-count-right').text(countCorrectSouthAmerica);
-              $('.info-drawer[data-region="south-america"] .country-list').append('<li class="country-list-item">' + inputValue + '</li>');
-              $('.info-drawer[data-region="south-america"] .info-drawer-message').hide();
+              $('.info-drawer[data-region="south-america"] .country-list-item:eq(' + listSouthAmerica.indexOf(currentItem) + ')').append('<span class="country-list-item-text">' + inputValue + '</span>');
             } else if (currentItem.region == 'Europe') {
               countCorrectEurope += 1;
               $('.continent-list-item[data-region="europe"] .continent-count-right').text(countCorrectEurope);
-              $('.info-drawer[data-region="europe"] .country-list').append('<li class="country-list-item">' + inputValue + '</li>');
-              $('.info-drawer[data-region="europe"] .info-drawer-message').hide();
+              $('.info-drawer[data-region="europe"] .country-list-item:eq(' + listEurope.indexOf(currentItem) + ')').append('<span class="country-list-item-text">' + inputValue + '</span>');
             } else if (currentItem.region == 'Asia') {
               countCorrectAsia += 1;
               $('.continent-list-item[data-region="asia"] .continent-count-right').text(countCorrectAsia);
-              $('.info-drawer[data-region="asia"] .country-list').append('<li class="country-list-item">' + inputValue + '</li>');
-              $('.info-drawer[data-region="asia"] .info-drawer-message').hide();
+              $('.info-drawer[data-region="asia"] .country-list-item:eq(' + listAsia.indexOf(currentItem) + ')').append('<span class="country-list-item-text">' + inputValue + '</span>');
             } else if (currentItem.region == 'Oceania') {
               countCorrectOceania += 1;
               $('.continent-list-item[data-region="oceania"] .continent-count-right').text(countCorrectOceania);
-              $('.info-drawer[data-region="oceania"] .country-list').append('<li class="country-list-item">' + inputValue + '</li>');
-              $('.info-drawer[data-region="oceania"] .info-drawer-message').hide();
+              $('.info-drawer[data-region="oceania"] .country-list-item:eq(' + listOceania.indexOf(currentItem) + ')').append('<span class="country-list-item-text">' + inputValue + '</span>');
             } else if (currentItem.region == 'Africa') {
               countCorrectAfrica += 1;
               $('.continent-list-item[data-region="africa"] .continent-count-right').text(countCorrectAfrica);
-              $('.info-drawer[data-region="africa"] .country-list').append('<li class="country-list-item">' + inputValue + '</li>');
-              $('.info-drawer[data-region="africa"] .info-drawer-message').hide();
+              $('.info-drawer[data-region="africa"] .country-list-item:eq(' + listAfrica.indexOf(currentItem) + ')').append('<span class="country-list-item-text">' + inputValue + '</span>');
             }
 
             // Trigger a click to allow Google Maps to update (find a better solution for this later)
@@ -145,10 +161,6 @@ $(document).ready(function(){
               countDown();
               timerStatus = true;
             }
-
-
-            // Refresh the nano scrollers
-            $(".nano").nanoScroller();
           }
         }
       }
@@ -157,8 +169,6 @@ $(document).ready(function(){
 
 
   // Open country drawers by clicking on the continents
-
-  var continents = ['north-america', 'south-america', 'europe', 'asia', 'oceania', 'africa'];
 
   $.each(continents, function(key, value) {
     $('.continent-list-item[data-region="' + value + '"]').on('click', function () {
